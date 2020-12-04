82794ea4fc9f5cbae27a0447f71110b8b7c5a8c8
NavidHamidi <33211965+NavidHamidi@users.noreply.github.com>
2020-12-04 01:43:43 +0100

    Add files via upload

---
+++ App.js
@@ -0,0 +1,10 @@
+import { StatusBar } from 'expo-status-bar';
+import React from 'react';
+
+import MainMenu from './src/Navigation/MainMenu'
+
+export default function App() {
+  return (
+		<MainMenu/>
+  );
+}
